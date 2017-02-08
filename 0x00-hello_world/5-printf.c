22d4b614e9544ec9c0a3db2c154a884578a81afb
KRLoyd <135@holbertonschool.com>
2017-02-08 23:45:12 +0000

    correct comment and spaces

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
- * Retrun: Always 0 (Success)
- */
+ * Return: Always 0 (Success)
+**/
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return(0);
+	return (0);
 }
