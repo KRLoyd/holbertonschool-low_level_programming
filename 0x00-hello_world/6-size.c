9e1c9ef3311506cdb9b96e2e40e237b669953afd
KRLoyd <135@holbertonschool.com>
2017-02-09 00:49:35 +0000

    simplify script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,10 @@
 
 int main(void)
 {
-	char c;
-	int i;
-	long int li;
-	long long int lli;
-	float f;
-
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return(0);
 }
