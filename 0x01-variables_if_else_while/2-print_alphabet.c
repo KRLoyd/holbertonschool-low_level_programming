d8858f0d6d90df6c5d46099de9e5a78b567e406f
KRLoyd <135@holbertonschool.com>
2017-02-09 21:31:16 +0000

    create 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar (printf("\n"));
+	return (0);
+}
