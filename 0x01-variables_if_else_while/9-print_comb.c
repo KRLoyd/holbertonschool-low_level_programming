2f7f5113fb60bbb8d8c2ae50e03f7b5a2f9dbd45
KRLoyd <135@holbertonschool.com>
2017-02-10 00:17:34 +0000

    create 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int i;
+
+	i = '0';
+
+	while (i <= '8')
+	{
+		putchar(i);
+		putchar(',');
+		i++;
+	}
+	while (i == '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
