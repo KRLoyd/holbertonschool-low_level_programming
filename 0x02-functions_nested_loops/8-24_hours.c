99d4bc3ef17e7a6636d8561bcc68624da4b5063f
KRLoyd <135@holbertonschool.com>
2017-02-14 21:46:18 +0000

    create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prin every minute of Jack's day
+ *
+ **/
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	h = 00;
+
+	while (h <= 23)
+	{
+		m = 00;
+
+		while (m <= 59)
+		{
+			_putchar(h / 10 % 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 % 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
