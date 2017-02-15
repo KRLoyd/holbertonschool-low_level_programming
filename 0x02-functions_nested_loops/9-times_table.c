2f76130d98e476583da30fe47e00d06f10965012
KRLoyd <135@holbertonschool.com>
2017-02-15 01:03:36 +0000

    correct tabs on lines 23, 26, 28

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,12 +20,12 @@ void times_table(void)
 			k = a * b;
 			if (k <= 9)
 			{
-		       		_putchar(k + '0');
+				_putchar(k + '0');
 				if (b < 9)
 				{
-	       			_putchar(',');
-				_putchar(' ');
-	       			_putchar(' ');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 				}
 			}
 			else
