ee757932f884cdb16300ebbb8605dfee11b50fea
KRLoyd <135@holbertonschool.com>
2017-02-15 01:01:28 +0000

    create 9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ * times_table - print the 9 times table
+ *
+ **/
+
+void times_table(void)
+{
+	int a, b, k;
+
+	a = 0;
+
+	while (a <= 9)
+	{
+		b = 0;
+
+		while (b < 10)
+		{
+			k = a * b;
+			if (k <= 9)
+			{
+		       		_putchar(k + '0');
+				if (b < 9)
+				{
+	       			_putchar(',');
+				_putchar(' ');
+	       			_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+				if (b < 9)
+				{
+				_putchar(',');
+				}
+				_putchar(' ');
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -11,3 +11,4 @@ Various projects to work with variables and nested loops in C.
 8-24_hours.c -> Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59;
 10-add.c -> Write a function that adds two integer and returns the result;
 11-print_to_98.c -> Write a function that prints all natural numbers from n to 98, followed by a new line;
+9-times_table.c -> Write a function that prints the 9 times table, starting with 0;
