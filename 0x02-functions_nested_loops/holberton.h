8e85dd162c40b0d6c9f613346d1fc7d7f6f06831
KRLoyd <135@holbertonschool.com>
2017-02-15 01:00:56 +0000

    add prototype for times_table

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,6 @@
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -8,3 +9,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
 void print_to_98(int n);
+void times_table(void);
