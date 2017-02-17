0211fa5480e82af0a96dfefb4bfbc5eb07d8318f
KRLoyd <135@holbertonschool.com>
2017-02-17 03:36:05 +0000

    fix for 64b machine (4b ints)

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -8,29 +8,28 @@
 
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		n = -n;
-		_putchar('-');
-	}
-	if (n > 9999)
-	{
-		_putchar(n / 10000 + '0');
-	}
-	if (n > 999)
-	{
-		_putchar(n / 1000 % 10 + '0');
-	}
-	if (n > 99)
-	{
-		_putchar(n / 100 % 10 + '0');
-	}
-	if (n > 9)
-	{
-		_putchar(n / 10 % 10 + '0');
-	}
 	if (n >= 0)
-	{
-		_putchar(n % 10 + '0');
-	}
+		n = -n;
+	else
+		_putchar('-');
+	if (n <= -2147483648)
+		_putchar(n / -1000000000 + '0');
+	if (n <= -999999999)
+		_putchar(n / -100000000 % 10 + '0');
+	if (n <= -9999999)
+		_putchar(n / -10000000 % 10 + '0');
+	if (n <= -999999)
+		_putchar(n / -1000000 % 10 + '0');
+	if (n <= -99999)
+		_putchar(n / -100000 % 10 + '0');
+	if (n <= -9999)
+		_putchar(n / -10000 % 10 + '0');
+	if (n <= -999)
+		_putchar(n / -1000 % 10 + '0');
+	if (n <= -99)
+		_putchar(n / -100 % 10 + '0');
+	if (n <= -9)
+		_putchar(n / -10 % 10 + '0');
+	if (n <= 0)
+		_putchar(n * -1 % 10 + '0');
 }
