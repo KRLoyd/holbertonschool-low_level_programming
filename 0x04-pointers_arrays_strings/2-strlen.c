afda5c86cb177a24e931af103b10163a66aa043a
KRLoyd <135@holbertonschool.com>
2017-02-20 23:38:53 +0000

    create 2-strlen.c

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: string to evaluate
+ *
+ * Return: length of string
+ **/
+
+int _strlen(char *s)
+{
+	int i;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
