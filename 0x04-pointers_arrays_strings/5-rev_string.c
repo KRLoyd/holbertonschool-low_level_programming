d655f024417827caf711ff3c55ca7e6100d48c6a
KRLoyd <135@holbertonschool.com>
2017-02-21 23:50:33 +0000

    create 5-rev_string.c

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rev_string - function to reverse a string
+ * @s: string to reverse
+ *
+ **/
+
+void rev_string(char *s)
+{
+	int i, j;
+	int length;
+	int temp[1000];
+
+	while (s[length] != '\0')
+	{
+	       length++;
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		temp[i] = s[i];
+	}
+
+	for (i = 0, j = length - 1; i < length; i++, j--)
+	{
+		s[i] = temp[j];
+	}
+}
