113e98fddd49654874bf87fa479543d770eb16bb
KRLoyd <135@holbertonschool.com>
2017-02-23 15:39:59 -0800

    correct indents and trailing semicolons/statements

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,7 @@
 
 /**
  * _strcmp - compare two strings
- * @s: first string to compare
+ * @s1: first string to compare
  * @s2: second string to compare
  *
  * Return: negative if s < s2, positive if s > s2, 0 if s = s2
@@ -15,7 +15,9 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
-		    return (s1[i] - s2[i]);
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
 	return (0);
 }
