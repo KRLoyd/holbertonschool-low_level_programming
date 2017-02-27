2c13ee6182d64f91666e637ad2761af0c68f9bae
KRLoyd <135@holbertonschool.com>
2017-02-27 20:11:02 +0000

    create 0-memset

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: memory to be filled
+ * @b: constant byte to fill memory
+ * @n: number of bytes in memory
+ *
+ * Return: pointer to memory area of s
+ **/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+	return ((char *)s);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+More pointers, arrays, and string practice!!!
+
+0-memset.c -> write a function that fills memory with a constant byte;
