504e947c05ef378d7f68b521a869ce4906a90070
KRLoyd <135@holbertonschool.com>
2017-02-27 20:39:04 +0000

    create 1-memcpy.c

---
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _memcpy - funciton that copies memory area
+ * @dest: memory to be updated
+ * @src: memory to save to dest
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ **/
+
+char *_memcpy(char *dest, char *src, unsigned n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return ((char *)dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 More pointers, arrays, and string practice!!!
 
 0-memset.c -> write a function that fills memory with a constant byte;
+1- memcpy.c -> write a function that copies memory area;
