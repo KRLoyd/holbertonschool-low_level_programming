acb485bf40dc73f6759d6f3a38be7ff075620e5b
KRLoyd <135@holbertonschool.com>
2017-02-28 00:46:07 +0000

    create 4-strpbrk.c

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * _strlen - find the length of a string
+ * @s: string to evaluate
+ *
+ * Return: length of the string
+ **/
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * _strpbrk - locate the first of any byte s that matches a byte in accept
+ * @s: string to look at
+ * @accept: accepted bytes
+ *
+ * Return: pointer to byte in s that matches one in accept, or NULL if no match
+ **/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int length, i, j;
+
+	length = _strlen(s);
+
+	for (i = 0; i <= length; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (s + length);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@ More pointers, arrays, and string practice!!!
 1- memcpy.c -> write a function that copies memory area;
 2-strchr.c -> write a function that locates a character in a string;
 3-strspn.c -> write a function that gets the length of a prefix substring;
+4-strpbrk.c -> write a function hat searches a string for any of a set of bytes;
