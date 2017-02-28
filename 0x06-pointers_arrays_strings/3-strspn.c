d36a0c8eaf7855189ba9520a80fd6adbd29341c9
KRLoyd <135@holbertonschool.com>
2017-02-28 00:15:30 +0000

    create 3-strspn.c

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strspn - function to find the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: prefix substring to check for
+ *
+ * Return: bytes in the initial segment of s containing bytes from accept
+ **/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	int n;
+
+	n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				n++;
+				break;
+			}
+		}
+		if (s[i] != accept[j])
+		{
+			break;
+		}
+	}
+	return (n);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ More pointers, arrays, and string practice!!!
 0-memset.c -> write a function that fills memory with a constant byte;
 1- memcpy.c -> write a function that copies memory area;
 2-strchr.c -> write a function that locates a character in a string;
+3-strspn.c -> write a function that gets the length of a prefix substring;
