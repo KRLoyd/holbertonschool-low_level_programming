8751cfb205f943e61d48fc2963d0d0b6565121bc
KRLoyd <135@holbertonschool.com>
2017-02-28 01:44:41 +0000

    add code to account for haystack[i] == '\0' && needle[j] == '\0'

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -12,6 +12,10 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
+	if (needle == '\0' || haystack == '\0')
+	{
+		return ('\0');
+	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
