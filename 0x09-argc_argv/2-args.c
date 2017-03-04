0b098c2c98f8f7ed8fd0c18058b6c6b776547cdb
KRLoyd <135@holbertonschool.com>
2017-03-03 19:53:37 -0800

    create 2-args.c

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - entry point, print all arguments passed to main
+ * @argc: argument count
+ * @argv: strings passed to main
+ *
+ * Return: (0) Success
+ **/
+
+int main (int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
