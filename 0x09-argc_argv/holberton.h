405adfba03b4018677d50bb7f04b2b7d8842f0b9
KRLoyd <135@holbertonschool.com>
2017-03-05 16:55:11 -0800

    correct file to include #ifndef info

--- 0x09-argc_argv/holberton.h
+++ 0x09-argc_argv/holberton.h
@@ -1,4 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
 int _checkfornumber(char *c);
+#endif
