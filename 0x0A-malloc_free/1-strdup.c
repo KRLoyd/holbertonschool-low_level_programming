2e23b5f2e6a4e0cc04b4bd92e629f1d0f776ba32
KRLoyd <135@holbertonschool.com>
2017-03-07 23:22:33 +0000

    create 1-strdup

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - return the length of a string
+ * @s: string to evaluate
+ *
+ * Return: length of string
+ **/
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strdup - Entry Point
+ * @str: string to copy to newly allocated space
+ *
+ * Description: returns a pointer to newly allocated space in memory,
+ * contains copy of string given as parameter
+ * Return: pointer to duped string (Success), NULL (Failure)
+ **/
+
+char *_strdup(char *str)
+{
+	int j, len;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = _strlen(str);
+
+	if (len == 0)
+		return (NULL);
+
+	p = malloc((sizeof(char)) * len);
+
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	else
+	{
+		for (j = 0; j < len; j++)
+		{
+			p[j] = str[j];
+		}
+		return ((char *)p);
+	}
+}
