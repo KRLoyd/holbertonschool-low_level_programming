227067092fb03a781946537c787dd8514cd9b06f
KRLoyd <135@holbertonschool.com>
2017-03-08 00:52:31 +0000

    fix code: s1 != NULL to s1[i] != NULL, also for s2

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,76 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - return the length of a string
+ * @s: string to evaluate
+ *
+ * Return: length of a string
+ **/
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * str_concat - Entry Point
+ * @s1: first string
+ * @s2: second string
+ *
+ * Descrption: concatenate s2 to s1
+ * Return: pointer to newly allocated space with concatenated info (Success),
+ * NULL (Failure)
+ **/
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, len1, len2, totlen;
+	char *p;
+
+
+	if (s1 != NULL)
+		len1 = _strlen(s1);
+	else
+		len1 = 0;
+	if (s2 != NULL)
+		len2 = _strlen(s2);
+	else
+		len2 = 0;
+
+	totlen = len1 + len2 + 1; /*+1 to make an element at the end for '\0' */
+
+	p = malloc(totlen * sizeof(char));
+
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+
+	if (len1 != 0)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			p[i] = s1[i];
+		}
+	}
+	if (len2 != 0)
+	{
+		for (j = 0; s2[j] != '\0'; i++, j++)
+		{
+			p[i] = s2[j];
+		}
+       	}
+	p[i] = '\0';
+
+	return (p);
+}
