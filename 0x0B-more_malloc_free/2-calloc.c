06aefc964c8e6977d6cf04a4e5bba1edd3024646
KRLoyd <135@holbertonschool.com>
2017-03-09 22:47:28 +0000

    create 2-calloc.c

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate memory for an array of nmemb elements of size bytes
+ * @nmemb: number of elements in array
+ * @size: size in bytes of each element
+ *
+ * Return: pointer to newly allocated memory initialized to 0 (SUCCESS)
+ * NULL (FAILURE)
+ **/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, *ptr;
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i <= nmemb; i++)
+			ptr[i] = 0;
+	}
+	return (ptr);
+}
