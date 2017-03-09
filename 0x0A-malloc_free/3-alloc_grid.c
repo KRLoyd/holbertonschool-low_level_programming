98c483a2b0d78b402dba335307aefb937477b85d
KRLoyd <135@holbertonschool.com>
2017-03-09 00:22:41 +0000

    fix 3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -5,40 +5,38 @@
  * alloc_grid - returns a pointer to a 2D array of int, initialized at 0
  * @width: # of columns in 2D array
  * @height: # of rows in 2D array
- *
  * Return: pointer to 2D array (Success), NULL (Failure)
  **/
 
 int **alloc_grid(int width, int height)
 {
-	int i, j, k;
+	int i, j;
 	int **p;
-
-	i = 0;
-	j = 0;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	p = malloc(sizeof(int *) * height);
+	p = malloc(height * sizeof(int *));
 
 	if (p == NULL)
 	{
 		free(p);
 		return (NULL);
 	}
+
 	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(sizeof(int *) * width);
+		p[i] = malloc(width * sizeof(int));
 		if (p[i] == NULL)
 		{
 			/* clear memory already alloc for height */
-			for (k = i - 1; k >= 0; k--)
+			for (i = 0; i < height; i++)
+			{
 				free(p[i]);
+			}
 			free(p);
-			return (NULL);
 		}
 		for (j = 0; j < width; j++)
 		{
