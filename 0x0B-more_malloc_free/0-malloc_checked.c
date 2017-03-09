834fe2d03d24516e65a4f3164137f80f53d33777
KRLoyd <135@holbertonschool.com>
2017-03-09 20:38:29 +0000

    create 0-malloc_checked.c

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory in bytes to allocate
+ *
+ * Return: pointer to the address of the allocated memory (SUCCESS),
+ * Exit status 98 (FAILURE)
+ **/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		printf("NULL\n");
+		exit (98);
+	}
+	return (ptr);
+}
