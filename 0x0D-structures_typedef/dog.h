134583de954b548d8b7c4fc0d5d32835fb51c09e
KRLoyd <135@holbertonschool.com>
2017-03-13 15:24:56 -0700

    add prototypes to dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -13,4 +13,10 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* DOG_H */
