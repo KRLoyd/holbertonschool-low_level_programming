8f13fce3cd349fe4a19ec2efc11d8909e9e44967
KRLoyd <135@holbertonschool.com>
2017-03-15 10:50:40 -0700

    add comment block

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_name - function to print a name
+ * @name: name to print
+ * @f: function to print name with
+ *
+ * Return: void
+ **/
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
