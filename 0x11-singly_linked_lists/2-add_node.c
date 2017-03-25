c5cbea2500e64a8762d1892fb0b161647f0442c4
KRLoyd <135@holbertonschool.com>
2017-03-25 19:23:02 +0000

    add 2-add_node.c and prototypes

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * _strlen_const - return the length of a string
+ * @s: string to evlauate
+ *
+ * Return: length of string
+ **/
+
+int _strlen_const(const char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: first element in the list
+ * @str: string to be duplicated to new node
+ *
+ * Return: address of new element (SUCCESS), NULL (FAILURE)
+ **/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen_const(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
