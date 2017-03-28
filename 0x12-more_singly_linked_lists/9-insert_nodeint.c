b0af06ce98c1e2d4cd9a383b1ea06be30d038f57
KRLoyd <135@holbertonschool.com>
2017-03-28 20:52:40 +0000

    add if loop for idx != 0

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,8 +13,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
+	if (idx != 0)
+	{
+		if (head == NULL && *head == NULL)
+			return (NULL);
+	}
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
