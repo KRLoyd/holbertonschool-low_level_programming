372e7db61826021690e644f500e5644f2b5ff40a
KRLoyd <135@holbertonschool.com>
2017-03-29 18:04:41 +0000

    correct betty-style

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (i < (index -1))
+	while (i < (index - 1))
 	{
 		if (ptr->next == NULL)
 			return (-1);
@@ -47,5 +47,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	ptr->next = temp->next;
 	free(temp);
 
-  	return (1);
+	return (1);
 }
