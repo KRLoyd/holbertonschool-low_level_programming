c8db83afd54c2770ef24fd7f4d9762194c42dfc9
KRLoyd <135@holbertonschool.com>
2017-03-30 18:49:50 +0000

    add libraries

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,5 +1,8 @@
 #ifndef __HOLBERTON__
 #define __HOLBERTON__
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 #endif /* __HOLBERTON__ */
