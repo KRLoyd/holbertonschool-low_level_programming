26f168bd826a14cfd66c6e6bbe36523e5871eed8
KRLoyd <loydkristen@gmail.com>
2017-05-05 23:59:53 +0000

    remove printf statement

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -17,18 +17,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_last = malloc(sizeof(dlistint_t));
 	if (new_last == NULL)
 		return (NULL);
-	printf("after malloc space for new_last\n");
 /* add data to new_last */
 	new_last->n = n;
-	printf("new_last->n: %d\n", new_last->n);
 	new_last->prev = current;
 	new_last->next = NULL;
 
 /* check for empty list */
-	printf("Right before if check for empty list\n");
 	if (*head == NULL)
 	{
-		printf("Inside if check for empty list\n");
 		*head = new_last;
 	}
 /* make last node point to new node*/
