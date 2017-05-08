418c94f954f96b2270c7bcc6ee1baa842baf595f
KRLoyd <loydkristen@gmail.com>
2017-05-08 16:42:40 +0000

    Find number of nodes

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -9,12 +9,19 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 
+	unsigned int total_nodes;
+
+	printf("Before finding number of nodes\n");
 /* Find number of nodes in list */
+	total_nodes = dlistint_len(head);
+	printf("Total_nodes: %d\n", total_nodes);
 
 /* Compare index to number of nodes */
-
+	if (index > total_nodes)
+		printf("bigger");
 /* Traverse the list to index */
 
 /* Return node at index */
 
+	return (0);
 }
