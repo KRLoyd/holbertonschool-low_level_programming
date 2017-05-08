7cf3cffd5bf5a8328289f2503fdba199470007c0
KRLoyd <loydkristen@gmail.com>
2017-05-08 18:35:01 +0000

    Restart and add some comments

---
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes a node at a given position
+ * @head: double pointer to the first node in the list
+ * @index: node to delete
+ *
+ * Return: 1 (SUCCESS), -1 (FAILURE)
+ **/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+/* Check if head and *head are NULL */
+
+/* Traverse the list */
+
+/* If find index - 1 */
+/* If ptr != NULL */
+
+
+}
