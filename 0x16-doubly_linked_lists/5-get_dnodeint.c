a8a267d4fca28bb12789d7692b54994edbb760c6
KRLoyd <loydkristen@gmail.com>
2017-05-08 16:34:17 +0000

    edit return comment and starting function over

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -4,29 +4,9 @@
  * @head: pointer to the first node in the list
  * @index: index of the node (index begins at 0)
  *
- * Return: nth node of the list (SUCCESS), NULL (FAILURE)
+ * Return: nth node of the list, NULL if it doesn't exist
  **/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	size_t total_nodes;
-	dlistint_t *ptr;
-	unsigned int i;
 
-	i = 0;
-	ptr = head;
-/* find number of nodes in the list */
-	total_nodes = dlistint_len(head);
-
-/* check if index is out of range */
-	if (index > total_nodes)
-		return (NULL);
-
-/* Traverse list until index */
-	while (i < index)
-	{
-		ptr = ptr->next;
-		i++;
-	}
-/* return index node */
-	return (ptr);
 }
