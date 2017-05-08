5f4fb562d1bd80ad204913e020681a1fba0197d3
KRLoyd <loydkristen@gmail.com>
2017-05-08 16:50:18 +0000

    Check if index > total_nodes and return NULL if true

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -11,14 +11,19 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	unsigned int total_nodes;
 
+	printf("Index: %d\n", index);
 	printf("Before finding number of nodes\n");
 /* Find number of nodes in list */
 	total_nodes = dlistint_len(head);
 	printf("Total_nodes: %d\n", total_nodes);
 
+	printf("Before if comparing index to number of nodes\n");
 /* Compare index to number of nodes */
 	if (index > total_nodes)
-		printf("bigger");
+	{
+		printf("inside if statement\n");
+		return (NULL);
+	}
 /* Traverse the list to index */
 
 /* Return node at index */
