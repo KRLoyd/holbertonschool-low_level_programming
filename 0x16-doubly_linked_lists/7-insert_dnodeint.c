7ff54f198ab22ba81dd91f856113410d9a6418af
KRLoyd <loydkristen@gmail.com>
2017-05-08 18:16:15 +0000

    Add if stmt for idx == 0

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,31 +9,34 @@
  **/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-/*	size_t total_nodes;*/
 	dlistint_t *new_node;
 	dlistint_t *ptr;
 	unsigned int node_num;
 
 	ptr = *h;
 	node_num = 0;
-
 /* Check if h and *h are NULL */
 	if (h == NULL || *h == NULL)
-		return NULL;
+		return (NULL);
 /* Create new node */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-/* Add data to new node */ 
+/* Add data to new node */
 	new_node->n = n;
+/* if idx == 0 */
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		*h = new_node;
+		return (new_node);
+	}
 /* Traverse the linked list to get to idx */
 	while (ptr != NULL)
 	{
 /* If idx is found, update pointers and return */
 		if (idx == node_num + 1)
 		{
-			printf("ptr->n: %d\n", ptr->n);
-			printf("idx: %d\nnode_num: %d\n", idx, node_num);
 			new_node->prev = ptr->prev;
 			new_node->next = ptr->next;
 			ptr->next = new_node;
