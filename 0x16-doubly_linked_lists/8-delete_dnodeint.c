f54711c47f5b251ca4fbf6a88affb19479cff2b5
KRLoyd <loydkristen@gmail.com>
2017-05-08 20:29:55 +0000

    Add functionality if index is > 0 or last in the list

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,34 +10,42 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *node_reference;
 	dlistint_t *node_to_delete;
+	unsigned int node_num;
 
-	node_reference = *head;
-	node_to_delete = *head;
+	node_reference = node_to_delete = *head;
+	node_num = 0;
 /* Check if head and *head are NULL */
 	if (head == NULL || *head == NULL)
 		return (-1);
-/* If index == 0 */
-	printf("\tBefore check for index == 0\n");
 	if (index == 0)
-	{
-		printf("\tIndex == 0\n");
-		/* Is this the only link? */
+	{		/* Is this the only link? */
 		if (node_to_delete->next == NULL)
 		{
 			free(node_to_delete);
-			*head = NULL;
-			return (-1);
+			*head = NULL; return (-1);
 		}
 		else /* This is link 0 of more */
 		{
 			*head = node_to_delete->next;
 			(*head)->prev = NULL;
-			free(node_to_delete);
-			return (1);
+			free(node_to_delete); return (1);
 		}
 	}
-/* Is this the only link? */
 /* Traverse the list */
-/* Is it the last link? */
+	while (node_num < index - 1)
+	{
+		node_reference = node_reference->next;
+		if (node_reference == NULL)
+			return (-1);
+		node_num++;
+	}
+/* Update pointers to remove the node_to_delete */
+	node_to_delete = node_reference->next;
+	node_reference->next = node_to_delete->next;
+/* If node is not the last, make the next node's prev = node_reference */
+	if (node_to_delete->next != NULL)
+		node_to_delete->next->prev = node_reference;
+/* free the node that was "deleted" */
+	free(node_to_delete);
 	return (1);
 }
