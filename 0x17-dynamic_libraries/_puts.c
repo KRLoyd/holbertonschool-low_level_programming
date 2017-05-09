8ca9b2adc194af58e2db91f0e22c41df3480996c
KRLoyd <loydkristen@gmail.com>
2017-05-09 03:18:49 +0000

    create _puts.c

---
+++ 0x17-dynamic_libraries/_puts.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _puts - prints a string, followed by a new line to stdout
+ * @s: string to print
+ */
+void _puts(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+}
