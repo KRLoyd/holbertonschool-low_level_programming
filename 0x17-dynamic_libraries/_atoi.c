febb4bd734557eb9de4abe9406d9febe955742e0
KRLoyd <loydkristen@gmail.com>
2017-05-10 22:42:52 +0000

    Create fake file for atoi

---
+++ 0x17-dynamic_libraries/_atoi.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to evaluate
+ *
+ * Return: Always Negative 1, not functioning yet
+ */
+int _atoi(char *s)
+{
+	return (-1);
+}
