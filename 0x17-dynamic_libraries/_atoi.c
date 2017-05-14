ea70832083021a14b31419108cdae97df4a4e918
KRLoyd <loydkristen@gmail.com>
2017-05-14 22:07:01 +0000

    Add line to print string

--- 0x17-dynamic_libraries/_atoi.c
+++ 0x17-dynamic_libraries/_atoi.c
@@ -1,11 +1,13 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * _atoi - converts a string to an integer
  * @s: string to evaluate
  *
- * Return: Always Negative 1, not functioning yet
+ * Return: Always -1, not functioning yet
  */
 int _atoi(char *s)
 {
+	printf("%s\n", s);
 	return (-1);
 }
