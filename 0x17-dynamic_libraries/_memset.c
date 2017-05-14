c4c11229c315e4219e6d6c9baed6e7fdf42d4611
KRLoyd <loydkristen@gmail.com>
2017-05-14 21:05:28 +0000

    Create _memset.c

---
+++ 0x17-dynamic_libraries/_memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area to fill
+ * @b: byte to fill memory with
+ * @n: number of bytes to fill
+ *
+ * Return: pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
