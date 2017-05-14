f3d73bfd7c8b72d952bab0ddc6438798dcc2ee69
KRLoyd <loydkristen@gmail.com>
2017-05-14 21:59:44 +0000

    Correct prototype for _strspn

--- 0x17-dynamic_libraries/holberton.h
+++ 0x17-dynamic_libraries/holberton.h
@@ -18,6 +18,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char c);
+char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 #endif /* __HOLBERTON_8517__ */
