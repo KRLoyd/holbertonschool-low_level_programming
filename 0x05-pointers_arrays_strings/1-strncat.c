b6efdbc63f9313a087cdb8a574abee27f011300f
KRLoyd <loydkristen@gmail.com>
2017-05-25 09:01:53 +0000

    Add comments

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strncat - concatenate 2 strings to nth byte
--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -13,10 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+	/* Copy src to dest up to n or reaches '\0' */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	/* fill in the rest of dest's memory with '\0' */
 	for (; i < n; i++)
 	{
 		dest[i] = '\0';
