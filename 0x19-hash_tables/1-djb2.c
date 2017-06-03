70c7513e45c7770c1b200fd470927d70545f5137
KRLoyd <loydkristen@gmail.com>
2017-06-03 11:29:53 +0000

    Add description

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,4 +1,10 @@
 #include "hash_tables.h"
+/**
+ * hash_djb2 - hash function that implements the djb2 algorithm
+ *
+ * @str: Key to evaluate
+ * Return: unsigned long int associated with input key
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
