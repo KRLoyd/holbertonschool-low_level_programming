4861d311c119200006716dca9b73426ad350db05
KRLoyd <loydkristen@gmail.com>
2017-06-03 11:18:58 +0000

    Create/modify files for task 1: 1-djb2.c - created main-1.c - created, to run 1.djb2.c hash_tables.h - prototype for hash_djb2 added

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -32,4 +32,5 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 #endif /* __HASH_TABLES_6317__ */
