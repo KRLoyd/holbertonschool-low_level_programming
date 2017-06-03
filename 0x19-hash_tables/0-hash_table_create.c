836cae2b3c866f9e1412bdb86d6a8ec4998534d5
KRLoyd <loydkristen@gmail.com>
2017-06-03 11:05:42 +0000

    Create files for task 0: 0-hash_table_create.c main-0.c: to use 0-hash_table_create.c

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creates a hash table of specified size
+ *
+ * @size: size of the array
+ * Returns: pointer to new hash_table (SUCCESS), NULL (FAILURE)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_ht;
+
+	/* Check if size is 0 */
+	if (size == 0)
+		return (NULL);
+
+	/* Allocate  space for the hash_table */
+	new_ht = malloc(sizeof(hash_table_t) * size);
+	if (new_ht == NULL)
+		return (NULL);
+
+	/* Allocate space for the array */
+	new_ht->array = calloc(size, sizeof(hash_node_t *));
+	if (new_ht->array == NULL)
+	{
+		free(new_ht);
+		return (NULL);
+	}
+	new_ht->size = size;
+	return (new_ht);
+}
