e04db61c24e0d84010cbcd7394fb5bdd949aac7a
KRLoyd <loydkristen@gmail.com>
2017-06-03 11:22:48 +0000

    Add check (c = *str++) != 0

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -3,8 +3,10 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash;
 	int c;
 
+
+
 	hash = 5381;
-	while ((c = *str++))
+	while ((c = *str++) != 0)
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
