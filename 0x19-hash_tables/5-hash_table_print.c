2e66fcef72e0dc04dab761f7b2f536c5f33127b8
KRLoyd <loydkristen@gmail.com>
2017-06-05 22:57:16 +0000

    Create/modify files for task 5: 5-hash_table_print.c - create main-5.c - create to use hash_table_print()

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -6,13 +6,13 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned int index, used_table_size;
+	unsigned int index;
+	int used_table_size;
 	hash_node_t *list_ptr;
 	hash_node_t *array_ptr;
 
 	if (ht == NULL)
 		return;
-
 	/* find number of non-NULL lists in array */
 	for (used_table_size = 0, index = 0, array_ptr = ht->array[index];
 	     index < ht->size;)
@@ -28,11 +28,9 @@ void hash_table_print(const hash_table_t *ht)
 		printf("\n");
 		return;
 	}
-	/* toop through array of lists is not all NULL */
+	/* loop through array of lists is not all NULL */
 	for (index = 0; index < ht->size; index++)
 	{
-		if (ht->array[index] != NULL)
-			used_table_size -= 1;
 		if (index == 0)
 			printf("{");
 		/* loop through the nodes in the linked list */
@@ -40,10 +38,12 @@ void hash_table_print(const hash_table_t *ht)
 		     list_ptr != NULL; list_ptr = list_ptr->next)
 		{
 			printf("'%s': '%s'", list_ptr->key, list_ptr->value);
-			if (used_table_size > 0)
+			if (used_table_size > 0 && list_ptr->next != NULL)
 				printf(", ");
 		}
 		if (index == ht->size - 1)
 			printf("}\n");
+		if (ht->array[index] != NULL)
+			used_table_size -= 1;
 	}
 }
--- 0x19-hash_tables/my_mains/main-5.c
+++ 0x19-hash_tables/my_mains/main-5.c
@@ -13,13 +13,15 @@ int main(void)
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
-	hash_table_set(ht, "c", "fun");
-	hash_table_set(ht, "python", "awesome");
-	hash_table_set(ht, "Jennie", "and Jay love asm");
-	hash_table_set(ht, "N", "queens");
-	hash_table_set(ht, "Asterix", "Obelix");
-	hash_table_set(ht, "Betty", "Holberton");
-	hash_table_set(ht, "98", "Battery Street");
+	/* hash_table_set(ht, "c", "fun"); */
+	/* hash_table_set(ht, "python", "awesome"); */
+	/* hash_table_set(ht, "Jennie", "and Jay love asm"); */
+	/* hash_table_set(ht, "N", "queens"); */
+	/* hash_table_set(ht, "Asterix", "Obelix"); */
+	/* hash_table_set(ht, "Betty", "Holberton"); */
+	/* hash_table_set(ht, "98", "Battery Street"); */
+	hash_table_set(ht, "stylist", "original collision");
+	hash_table_set(ht, "subgenera", "new collision");
 	hash_table_print(ht);
 	return (EXIT_SUCCESS);
 }
