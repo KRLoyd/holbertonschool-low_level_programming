68d5c4bfdb60916443496a9eaa8ed06fa1185372
KRLoyd <loydkristen@gmail.com>
2017-06-06 00:05:24 +0000

    Update .c file - remove comments and comply with betty style update .h - add prototype for 6

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -6,49 +6,41 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned int index;
+	unsigned int i;
 	int used_table_size, checker;
-	hash_node_t *list_ptr;
+	hash_node_t *l_ptr;
 	hash_node_t *array_ptr;
-
 
 	if (ht == NULL)
 		return;
-
 	checker = 0;
-	/* find number of non-NULL lists in array */
-	for (used_table_size = 0, index = 0, array_ptr = ht->array[index];
-	     index < ht->size;)
+	for (used_table_size = 0, i = 0; i < ht->size;)
 	{
+		array_ptr = ht->array[i];
 		if (array_ptr != NULL)
 			used_table_size++;
-		index++;
-		array_ptr = ht->array[index];
+		i++;
+		array_ptr = ht->array[i];
 	}
-	/* check if all lists are NULL */
 	if (used_table_size == 0)
 	{
 		printf("\n");
 		return;
 	}
-	/* loop through array of lists is not all NULL */
-	for (index = 0; index < ht->size; index++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (index == 0)
+		if (i == 0)
 			printf("{");
-		if (ht->array[index] != NULL)
+		if (ht->array[i] != NULL)
 			used_table_size -= 1;
-
-		/* loop through the nodes in the linked list */
-		for (list_ptr = ht->array[index];
-		     list_ptr != NULL; list_ptr = list_ptr->next)
+		for (l_ptr = ht->array[i]; l_ptr != NULL; l_ptr = l_ptr->next)
 		{
 			if (checker == 1)
 				printf(", ");
-			printf("'%s': '%s'", list_ptr->key, list_ptr->value);
+			printf("'%s': '%s'", l_ptr->key, l_ptr->value);
 			checker = 1;
 		}
-		if (index == ht->size - 1)
+		if (i == ht->size - 1)
 			printf("}\n");
 	}
 }
--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -43,4 +43,6 @@ hash_node_t *add_hash_node(hash_node_t *ptr,
 			   const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+void free_list(hash_node_t *ptr);
 #endif /* __HASH_TABLES_6317__ */
