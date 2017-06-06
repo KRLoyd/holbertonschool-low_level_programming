c3fdca6d076161f60f3c8765b7b85a6d94a42f3a
KRLoyd <loydkristen@gmail.com>
2017-06-06 16:27:50 +0000

    Moved all main files to ./my_mains/

--- 0x19-hash_tables/my_mains/main-6.c
+++ 0x19-hash_tables/my_mains/main-6.c
@@ -11,29 +11,34 @@
 int main(void)
 {
 	hash_table_t *ht;
+	char *key;
+	char *value;
 
 	ht = hash_table_create(1024);
-
-	/* Test Case 0: Print Empty Has Table */
-	/* hash_table_print(ht); */
-
-	/* Test Case 0: All NULL */
-	/* ht = NULL; */
-	/* hash_table_print(ht); */
-
-	/* Test Case 1: Single Nodes */ 
 	hash_table_set(ht, "c", "fun");
 	hash_table_set(ht, "python", "awesome");
 	hash_table_set(ht, "Jennie", "and Jay love asm");
 	hash_table_set(ht, "N", "queens");
 	hash_table_set(ht, "Asterix", "Obelix");
 	hash_table_set(ht, "Betty", "Holberton");
-	hash_table_set(ht, "98", "Battery Street");
-	hash_table_print(ht);
 
-	/* Test Case 2: A Chain */
-	hash_table_set(ht, "stylist", "original collision");
-	hash_table_set(ht, "subgenera", "new collision");
+	hash_table_set(ht, "98", "Battery Streetz");
+
+	key = strdup("Tim");
+	value = strdup("Britton");
+	hash_table_set(ht, key, value);
+	key[0] = '\0';
+	value[0] = '\0';
+	free(key);
+	free(value);
+	hash_table_set(ht, "98", "Battery Street"); 
+
+	hash_table_set(ht, "hetairas", "Jennie");
+	hash_table_set(ht, "hetairas", "Jennie Z");
+
+	hash_table_set(ht, "mentioner", "Jennie");
+	hash_table_set(ht, "hetairas", "Jennie Z Chu");
 	hash_table_print(ht);
+	hash_table_delete(ht);
 	return (EXIT_SUCCESS);
 }
