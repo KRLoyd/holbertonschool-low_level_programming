0478904e21812474563d86c830dd34371c84515f
KRLoyd <loydkristen@gmail.com>
2017-07-25 17:54:27 +0000

    Change check to be if tree == NULL

--- 0x1C-binary_trees/3-binary_tree_delete.c
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -7,13 +7,10 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	/* if (tree == NULL) */
-	/* 	return; */
+	if (tree == NULL)
+		return;
 
-	if (tree != NULL)
-	{
-		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-		free(tree);
-	}
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
