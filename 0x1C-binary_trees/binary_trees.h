b31504aabe59df4783f455ef80f65cc01450adaa
KRLoyd <loydkristen@gmail.com>
2017-07-25 00:28:21 +0000

    Add declaration of 0-binary_tree_node

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -34,6 +34,7 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 
 /* Declare task functions */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 /* Declare helper functions */
 
