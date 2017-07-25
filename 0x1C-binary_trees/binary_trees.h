aa765fc2268a18d0422824129b50f49fd0d6eabf
KRLoyd <loydkristen@gmail.com>
2017-07-25 00:07:01 +0000

    First commit for header file binary_trees.h Includes definitions for struct for binary trees, provided by Holberton. Also includes definition for binary_tree_print (to be used while working on tasks)

---
+++ 0x1C-binary_trees/binary_trees.h
@@ -0,0 +1,40 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+/* Headers for binary_tree_print */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/* Headers for all other functions */
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/* Declare binary_tree_print */
+void binary_tree_print(const binary_tree_t *);
+
+/* Declare task functions */
+
+/* Declare helper functions */
+
+#endif /* _BINARY_TREES_H_ */
