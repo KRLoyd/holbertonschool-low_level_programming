01251bef16f6f459697ef6e91537746fd1ddf8aa
KRLoyd <loydkristen@gmail.com>
2017-07-26 22:51:41 +0000

    Add prototypes for binary_tree_balance and max_tree_height

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -48,8 +48,10 @@ size_t binary_tree_depth(const binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 
 /* Declare helper functions */
+size_t max_tree_height(const binary_tree_t *tree);
 
 
 #endif /* _BINARY_TREES_H_ */
