cf2c2c60b08239f2d75d109257858839b3208884
KRLoyd <loydkristen@gmail.com>
2017-07-26 20:11:28 +0000

    Create function to find the depth of a node

---
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - function to measure the depth of a node in a binary tree
+ *
+ * @node: pointer to the node to measure the depth of
+ * Return: depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	const binary_tree_t *ptr;
+	size_t depth;
+
+	ptr = NULL;
+	depth = 0;
+
+	if (node == NULL)
+		return (0);
+
+	/* for (ptr = node; ptr->parent != NULL; ptr = ptr->parent) */
+	/* { */
+	/* 	depth++; */
+	/* } */
+
+	ptr = node;
+	while (ptr->parent != NULL)
+	{
+		depth++;
+		ptr = ptr->parent;
+	}
+
+	return (depth);
+}
