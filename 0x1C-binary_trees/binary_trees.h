fa8142cdea194c5f3e80763b6f01c6fc0adf1923
KRLoyd <loydkristen@gmail.com>
2017-07-26 22:13:45 +0000

    Add prototype for binary_trees_leaves

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -46,6 +46,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
 /* Declare helper functions */
 
 
