1edc70aed8aba154d52a7c337b0b67baa78c3a36
KRLoyd <loydkristen@gmail.com>
2017-07-28 21:08:14 +0000

    Main file to test binary_tree_uncle

---
+++ 0x1C-binary_trees/main_files/18-main.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	binary_tree_t *root;
+	binary_tree_t *uncle;
+
+	root = binary_tree_node(NULL, 98);
+	root->left = binary_tree_node(root, 12);
+	root->right = binary_tree_node(root, 128);
+	root->left->right = binary_tree_node(root->left, 54);
+	root->right->right = binary_tree_node(root->right, 402);
+	root->left->left = binary_tree_node(root->left, 10);
+	root->right->left = binary_tree_node(root->right, 110);
+	root->right->right->left = binary_tree_node(root->right->right, 200);
+	root->right->right->right = binary_tree_node(root->right->right, 512);
+	root->right->right->right->right = binary_tree_node(root->right->right->right, 70000);
+
+	binary_tree_print(root);
+	uncle = binary_tree_uncle(root->left);
+	printf("Uncle of %d: %p\n", root->left->n, (void *)uncle);
+	uncle = binary_tree_uncle(root->right->left);
+	printf("Uncle of %d: %d\n", root->right->left->n, uncle->n);
+	uncle = binary_tree_uncle(root->left->right);
+	printf("Uncle of %d: %d\n", root->left->right->n, uncle->n);
+	uncle = binary_tree_uncle(root);
+	printf("Uncle of %d: %p\n", root->n, (void *)uncle);
+
+
+	uncle = binary_tree_uncle(root->right->right->right->right);
+	printf("Uncle of %d: %d\n", root->right->right->right->right->n, uncle->n);
+
+	uncle = binary_tree_uncle(NULL);
+	printf("Uncle of NULL: %p\n", (void *)uncle);
+
+	free(uncle);
+	binary_tree_delete(root);
+	return (0);
+}
