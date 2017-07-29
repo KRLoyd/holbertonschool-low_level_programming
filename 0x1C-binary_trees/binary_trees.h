fe708cffd74d92185609e86bf8678e6814aaba0d
KRLoyd <loydkristen@gmail.com>
2017-07-29 00:27:17 +0000

    Add prototype for leaf_check

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -53,9 +53,14 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
+
 
 /* Declare helper functions */
 size_t max_tree_height(const binary_tree_t *tree);
-
+int leaf_check(const binary_tree_t *tree,
+	       unsigned int *currentlevel,
+	       unsigned int depthlevel);
 
 #endif /* _BINARY_TREES_H_ */
