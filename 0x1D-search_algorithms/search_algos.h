80eb212fa24ebee53e453217810e55616ee6e7f2
KRLoyd <loydkristen@gmail.com>
2017-08-31 20:42:34 +0000

    Add prototypes for task 1

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -1,4 +1,10 @@
 #ifndef __SEARCH_ALGOS_H__
 #define __SEARCH_ALGOS_H__
+
+#include <stdio.h>
+
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int rec_bs(int *array, int low, int high, int value);
+void print_array(int *array, int low, int high);
 #endif /* __SEARCH_ALGOS_H__ */
